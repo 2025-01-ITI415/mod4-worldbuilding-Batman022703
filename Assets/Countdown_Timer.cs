7ace14ba7c87f0a2d9ce471b87ac4f79905f5e63
Batman022703 <patrick.lutz@rutgers.edu>
2025-05-07 18:36:57 -0400

    Many changes
    Almost done with my project

---
+++ Assets/Countdown_Timer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown_Timer : MonoBehaviour
+{
+
+private float endgametime; 
+public float numberofminutes= 5f; 
+private Text timertext;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     endgametime = Time.time + (numberofminutes*60); 
+     timertext= GetComponent< Text> (); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     timertext.text = (endgametime - Time.time).ToString("#.00");    
+    }
+}
